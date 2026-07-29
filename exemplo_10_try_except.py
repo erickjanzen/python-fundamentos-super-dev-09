b247f94a0fd3d1a9a9d402adb97675234a437afe
Erick Janzen <erickjanzen554@gmail.com>
2026-07-29 19:42:44 -0300

    exemplos de try except

---
+++ exemplo_10_try_except.py
@@ -0,0 +1,30 @@
+def exemplo_sem_tratamento():
+    print("Divisão: ", 10 / 0)
+    print("Mensagem depois da divisão")
+    # lança a excessão: ZeroDivisionError: division by zero
+
+def exemplo_com_tratamento():
+    try:
+        print("Divisão: ", 10 / 0)
+    except ZeroDivisionError:
+        print("Não é possível dividir um número por zero.")
+
+    print("O programa continuou normalmente")
+
+
+def exemplo_com_tratamento_conversao():
+    numero_digitado: str = "dois"
+    try:
+        # converter de str para int
+        numero: int = int(numero_digitado)
+        print("Número digtado: ", numero)
+    except ValueError:
+        print("Texto digitado não é um número válido")
+        # print("Não foi possível converter o número para inteiro")
+    print("Acabou")
+
+# ponto de entrada da aplicação, deve ter um único da aplicação inteira
+if __name__ == "__main__":
+    # exemplo_sem_tratamento()
+    # exemplo_com_tratamento()
+    exemplo_com_tratamento_conversao()
